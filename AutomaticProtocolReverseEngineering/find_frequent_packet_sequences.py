2d7adacc0ee218d7006a2921063609430fc7b927
ehfdkdhswlgh <ehfdkdhswlgh@naver.com>
2023-05-11 04:09:57 +0900

    길이가 긴 시퀀스를 찾을 수 있도록 수정

--- AutomaticProtocolReverseEngineering/find_frequent_packet_sequences.py
+++ AutomaticProtocolReverseEngineering/find_frequent_packet_sequences.py
@@ -31,14 +31,49 @@ def find_frequent_packet_sequences(hex_string_list, min_acc, max_acc, length):
             # Calculate the new frequency percentage based on the actual packet occurrences
             freq_percentage = (len(packet_indices) / n) * 100
             if min_acc * 100 <= freq_percentage <= max_acc * 100:
-                D.append({
-                    "length": length,
-                    "The frequent sequence": hex_sequence,
-                    "Frequency": f"{freq_percentage:.1f}%",
-                })
+                # Check if the current sequence can be merged with the previous sequence
+                if D and can_be_merged(D[-1]["The frequent sequence"], hex_sequence, hex_string_list):
+                    merged_sequence = merge_sequences(D[-1]["The frequent sequence"], hex_sequence)
+                    D[-1]["The frequent sequence"] = merged_sequence
+                    D[-1]["length"] = len(merged_sequence) * 4
+                    # Update Packet Indices for the merged sequence
+                    D[-1]["Packet Indices"] = list(set(D[-1]["Packet Indices"]).union(packet_indices))
+                else:
+                    D.append({
+                        "length": length,
+                        "The frequent sequence": hex_sequence,
+                        "Frequency": f"{freq_percentage:.1f}%",
+                        "Packet Indices": packet_indices,
+                    })
                 seen_sequences.add(hex_sequence)
 
     return D, packet_indices_dict
+
+
+
+
+def can_be_merged(prev_sequence, curr_sequence, hex_string_list):
+    merged_sequence = merge_sequences(prev_sequence, curr_sequence)
+    merged_count = sum(1 for hex_string in hex_string_list if merged_sequence.lower() in hex_string.lower())
+
+    prev_count = sum(1 for hex_string in hex_string_list if prev_sequence.lower() in hex_string.lower())
+    curr_count = sum(1 for hex_string in hex_string_list if curr_sequence.lower() in hex_string.lower())
+
+    n = len(hex_string_list)
+    merged_freq = (merged_count / n) * 100
+    prev_freq = (prev_count / n) * 100
+    curr_freq = (curr_count / n) * 100
+
+    return prev_freq >= merged_freq >= curr_freq
+
+
+def merge_sequences(seq1, seq2):
+    overlap = 0
+    for i in range(len(seq1)):
+        if seq1[i:] == seq2[:len(seq1) - i]:
+            overlap = len(seq1) - i
+            break
+    return seq1 + seq2[overlap:]
 
 
 def bytearray_to_bin(byte_array):
@@ -48,4 +83,7 @@ def bytearray_to_bin(byte_array):
 def increment_counter(state_tree, sequence):
     state_tree[sequence] += 1
     return state_tree
+
+
+
 
